3d7cadb3911d67a91468a4a2ab9be902ace4de35
developer-vsandhu <77821789+developervinay1@users.noreply.github.com>
2023-08-02 09:04:35 +0530

    GSAP LEARNING

---
+++ script.js
@@ -0,0 +1,18 @@
+// gsap.to("#pageBox", {
+//   x: 700,
+//   duration: 2,
+//   delay: 2,
+//   backgroundColor: "#900",
+//   rotate: 90,
+//   scale: 1.5,
+// });
+
+// gsap.from("#pageBox", {
+//   x: 700,
+//   y: 300,
+//   duration: 2,
+//   delay: 2,
+//   backgroundColor: "#900",
+//   rotate: 90,
+//   scale: 1.5,
+// });
