b18d1014766cb424ef112452548a7307a56db329
developer-vsandhu <77821789+developervinay1@users.noreply.github.com>
2023-08-02 12:03:00 +0530

    Scroll Trigger Animation GSAP

--- script.js
+++ script.js
@@ -17,10 +17,23 @@
 //   scale: 1.5,
 // });
 
-gsap.to("#h1, #h2, #h3", {
-  color: "#ae0000",
-  x: 900,
-  y: 300,
-  duration: 2,
-  stagger: 1,
+// gsap.to("#h1, #h2, #h3", {
+//   color: "#ae0000",
+//   x: 900,
+//   y: 300,
+//   duration: 2,
+//   stagger: 1,
+// });
+
+gsap.to("#page2 h1", {
+  scale: 4,
+  opacity: 1,
+  duration: 1,
+  x: -200,
+  scrollTrigger: {
+    trigger: "#page2 h1",
+    scroller: "body",
+    scrub: 5,
+    markers: true,
+  },
 });
