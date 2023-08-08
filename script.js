1ae55bfdfa16267ef59f54355ad75011a59eba3d
developer-vsandhu <77821789+developervinay1@users.noreply.github.com>
2023-08-08 20:07:00 +0530

    Learning

--- script.js
+++ script.js
@@ -1,42 +1,4 @@
 gsap.registerPlugin(ScrollTrigger);
-// (function loco() {
-//   let locoScroll;
-//   locoScroll = new LocomotiveScroll({
-//     el: document.querySelector("#wrapper"),
-//     smooth: true,
-//     smartphone: {
-//       smooth: true,
-//     },
-//   });
-//   new ResizeObserver(() => locoScroll.update()).observe(
-//     document.querySelector("#wrapper")
-//   );
-
-//   locoScroll.on("scroll", ScrollTrigger.update);
-
-//   ScrollTrigger.scrollerProxy("#wrapper", {
-//     scrollTop(value) {
-//       return arguments.length
-//         ? locoScroll.scrollTo(value, 0, 0)
-//         : locoScroll.scroll.instance.scroll.y;
-//     },
-//     getBoundingClientRect() {
-//       return {
-//         top: 0,
-//         left: 0,
-//         width: window.innerWidth,
-//         height: window.innerHeight,
-//       };
-//     },
-//     pinType: document.querySelector("#wrapper").style.transform
-//       ? "transform"
-//       : "fixed",
-//   });
-
-//   ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-//   ScrollTrigger.refresh();
-// })();
 
 var tl = gsap.timeline();
 
@@ -78,7 +40,7 @@ tl.to(
       scroller: "body",
       duration: 0.5,
       scrub: 2,
-      pin: true,
+      // pin: true,
     },
     backgroundColor: "#FFD300",
   },
